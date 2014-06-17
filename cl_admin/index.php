3085ecc301cafca622f6fc93ee7097934a92dc35
clms2 <18067087965@163.com>
2014-06-17 17:19:29 +0800

    index.php
    iframe加载目标页,如果已经加载过就直接显示

--- cl_admin/index.php
+++ cl_admin/index.php
@@ -8,6 +8,7 @@ include_once 'check.php';
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 	<title>您好，欢迎登陆</title>
 	<link rel="stylesheet" href="css/base.css" />
+	<script src="js/jq.js"></script>
 </head>
 <body>
 	<div class="header">
@@ -17,10 +18,33 @@ include_once 'check.php';
 			<li class="nobg"><img src="images/icon_help.gif" width="16" height="16" alt="" /><a href="">帮助</a></li>
 		</ul>
 		<i></i>
+		<div class="clear"></div>
+		<ul class="nav_top clear lifl left" id="nav_top">
+			<li class="in"><a href="main_index.php" target="main_index">首页</a></li>
+			<li><a href="main_column.php" target="main_column">栏目管理</a></li>
+			<li><a href="" target="main">文章管理</a></li>
+		</ul>
 	</div>
 
-	<div class="left clear">
-		
+	<div class="main clearfix">
+		<div class="nav_left left">
+			
+		</div>
+		<div class="main_con left" id="main_con">
+		</div>
+		<script>
+		$("#nav_top a").click(function() {
+			var ifname = $(this).attr('target');
+			var container = $("#main_con iframe[name="+ifname+"]");
+			if(container.length == 0){
+				$('<iframe src="" frameborder="0" name="'+ifname+'"></iframe>').appendTo("#main_con").siblings().hide();
+			}else{
+				container.show().siblings().hide();
+				return false;
+			}
+		});
+		</script>
 	</div>
+
 </body>
 </html>
